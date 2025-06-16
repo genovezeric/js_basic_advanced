ffa57edfd7cb69d913c8d5e61a4cd9b1d637a7a5
eric.genovez <eric.genovez@britania.com.br>
2025-06-16 15:49:19 -0300

    Mais sobre arrays

---
+++ 4_array_e_objetos/12_shift_unshift/script.js
@@ -0,0 +1,8 @@
+let carros = ["BMW", "AUDI","VW", "FIAT"]
+
+let removerPrimeiroCarro = carros.shift()
+console.log(removerPrimeiroCarro)
+console.log(carros)
+
+carros.unshift("GW")
+console.log(carros)
